08cad08c95ff90d9741b2d0bca7fc89e77abbd8f
Paraskevi <paraskevi.vorropoulou@campus.tu-berlin.de>
2020-09-05 15:25:29 +0200

    DC_Toymodel producer and consumer based. Error fixed.

--- src/dc_dynamics.jl
+++ src/dc_dynamics.jl
@@ -1,19 +1,11 @@
 begin
-	using Random # random numbers
 	using LightGraphs # create network topologies
 	using LinearAlgebra
 	using DifferentialEquations
 	using GraphPlot
-	using DSP
 	using Plots
 	using Parameters
-	using ToeplitzMatrices
-	using JLD2, FileIO, GraphIO, CSV, DataFrames
-	using Distributed
-	using Interpolations
-	using Distributions
 	using LSODA
-	Random.seed!(42)
 end
 
 begin
@@ -21,6 +13,7 @@ begin
 	num_days = 10
 	l_day = 3600*24
 end
+
 begin
 	graph = random_regular_graph(iseven(3N) ? N : (N-1), 3)
 end
@@ -36,26 +29,10 @@ end
 	n_prod
 	n_cons
 
-	#function parameterss(N, K, R, L_inv, C_inv, v_ref, incidence, n_prof, n_cons)
-		#new(N, K, R, L_inv, C_inv, v_ref, incidence, n_prof, n_cons)
 end
-
-#@with_kw mutable struct fix
-#	fix_point
-#end
 
 ################# parameters initialization ###################################
 param = parameterss(N = 4, K = 1. , R = 0.0532, L_inv = 1/0.2, C_inv = 1/0.01,  v_ref = 48. , incidence = incidence_matrix(graph,oriented=true), n_prod = 2, n_cons = 2)
-#fixx = fix(fix_point = [0. 0. 0. 0. 0. 0. 48. 48. 48. 48.], )
-#N = 4
-#n_prod = 2
-#n_cons = N - n_prod
-#K = ones(n_prod)
-#R = 0.0532 .* ones(Int(1.5*N))
-#v_ref = (48.) .* ones(n_prod)
-#L_inv= 1/0.237e-4
-#C_inv = 1/0.01
-#incidence = incidence_matrix(graph,oriented=true)
 ###############################################################################
 
 
@@ -70,58 +47,36 @@ end
 
 function DCToymodel!(du, u, p, t)
    #DC Microgrids swarm type network implemented by Lia Strenge in python, equations 4.12
+   #print("du",du)
    P = -12.
    n_lines = Int(1.5*p.N)
-
+   #print(u)
 
    i = u[1:n_lines]
    v = u[(n_lines+1):end]
-	 #di = du[1:n_lines]
-	 #dv = du[(n_lines+1):end]
-	 #i = view(u, 1:Int(1.5*p.N)) # u = n_lines
+   di = @view du[1:n_lines]
+   dv = @view du[(n_lines+1):end]
+   #i = view(u, 1:Int(1.5*p.N)) # u = n_lines
    #v = view(u, (Int(1.5*p.N)+1):Int(2.5*p.N))
    #di = view(du, 1:Int(1.5*p.N))
    #dv = view(du, (Int(1.5*p.N)+1):Int(2.5*p.N))
+   inc_v = p.incidence' * v
+   inc_i = p.incidence * i
 
-   di = p.L_inv.*(p.incidence' * v .-p.R.*i)
-   dv = -1. .* (p.incidence * i)
+   @. di .= p.L_inv.*(inc_v .-(p.R.*i))
+   @. dv .= -1. .* inc_i
+   @. dv[1:p.n_prod] += p.K .* (p.v_ref.- v[1:p.n_prod])
+   @. dv[p.n_prod+1:end] += P ./ (v[p.n_prod+1:end].+1)
+   @. dv .*= p.C_inv
 
-   dv[1:p.n_prod] += p.K .* (p.v_ref.- v[1:p.n_prod])
-   dv[p.n_prod+1:end] += P ./ (v[p.n_prod+1:end].+1)
-   dv .*= p.C_inv
    return nothing
 end
 
-#function DCToymodel_parameters_in!(du, u, p, t)
-#   #DC Microgrids swarm type network implemented by Lia Strenge in python, equations 4.12
-#   P = -12.
-#   #print(u)
-#   i = view(u, 1:Int(1.5*4)) # u = n_lines
-#   v = view(u, (Int(1.5*4)+1):Int(2.5*4))
-#   #print(i)
-#   di = view(du, 1:Int(1.5*4))
-#   dv = view(du, (Int(1.5*4)+1):Int(2.5*4))
-#
-#
-#   di = 1/0.237E-04 * ([0. -1. 0. 1. ; -1. 0. 0. 1.; 0. 0. -1. 1.; 0. 1. -1. 0.; -1. 1. 0. 0.; -1. 0. 1. 0.]* v - (0.5237*i))
-#   dv = -1. * ([0. -1. 0. 0. -1. -1.; -1. 0. 0. 1. 1. 0.; 0. 0. -1. -1. 0. 1.; 1. 1. 1. 0. 0. 0.] * i)
-#
-#   dv[1:2] += (1.)* (48. .- v[1:2])
-#   dv[2+1:end] += (-12.) ./ (v[2+1:end].+1)
-#   dv .*= 1/0.01
-
-   #fix_point = ones(Int(2.5*p.N))
-
-
-
-#  return nothing
-#end
 
 ##### solving ###############################
-#initial condition
+
 begin
-	fp = [0. 0. 0. 0. 0. 0. 48. 48. 48. 48.]
-	#ic = 0.01 .* ones(5 * param.N)
+	fp = [0. 0. 0. 0. 0. 0. 48. 48. 48. 48.] #initial condition
 	tspan = (0. , num_days * l_day)
 	ode = ODEProblem(DCToymodel!, fp, tspan, param)
 end
